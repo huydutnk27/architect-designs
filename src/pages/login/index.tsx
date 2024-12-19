78f2c2e8ddbc32a8edda15ef2a3ffaff47512c4b
Du Nguyen Huy <Du.NguyenHuy@duluxgroup.com>
2024-12-19 17:41:13 +0700

    add new login folder

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { useAuth } from '../../hooks/useAuth';
+
+const Login = () => {
+    const { login } = useAuth();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Implement login logic (e.g., call login method from AuthContext)
+        login(username, password);
+    };
+
+    return (
+        <>
+            <div id="login-form">
+                <div className="form-bg">
+                    <div className="row">
+                        <div className="col-md-offset-4 col-md-4 col-sm-offset-3 col-sm-6">
+                            <div className="form-container">
+                                <form className="form-horizontal">
+                                    <h3 className="title">Login Form</h3>
+                                    <div className="form-group">
+                                        <span className="input-icon"><i className="fa fa-user"></i></span>
+                                        <input className="form-control" type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <span className="input-icon"><i className="fa fa-lock"></i></span>
+                                        <input className="form-control" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                    </div>
+                                    <button type="button" className="btn signin" onClick={handleLogin}>Log in</button>
+                                    <span className="forgot-pass"><a href="#">Lost password?</a></span>
+                                    <span className="register"><a href="#">Register / Signup</a></span>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
