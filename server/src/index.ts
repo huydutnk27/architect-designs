fb28486bc50a0bf82aab809546cbd46ceab15699
Du Nguyen Huy <Du.NguyenHuy@duluxgroup.com>
2024-12-19 18:46:44 +0700

    change server port

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ const path = require('path');
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const buildPath = path.join(__dirname, '../../build');
 
 // app.get("/", (req: Request, res: Response) => {
