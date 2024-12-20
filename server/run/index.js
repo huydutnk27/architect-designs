c927501d135eb1dc5997db3d1d2fbb9a5b4da7cb
Du Nguyen Huy <Du.NguyenHuy@duluxgroup.com>
2024-12-20 08:53:26 +0700

    Fix run duplicate server

--- server/run/index.js
+++ server/run/index.js
@@ -12,23 +12,20 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3001;
 const buildPath = path.join(__dirname, '../../build');
-// app.get("/", (req: Request, res: Response) => {
-//     res.send("Express + TypeScript Server");
-// });
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+
 // Middleware
 app.use(body_parser_1.default.json());
+
 app.use("/api", index_1.default);
 app.get('/api/test', async (_, res) => {
     res.json({ greeting: "Hello AAAAAAAAAA" });
 });
-// if (!process.env['VITE']) {
-// const frontendFiles = process.cwd() + '/dist'
 app.use(express_1.default.static(buildPath));
 app.get('/*', (_, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
 });
-app.listen(process.env['PORT']);
-// }
+
+// Start server
+app.listen(port, () => {
+    console.log(`[server]: Server is running at :${port}`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -10,18 +10,9 @@ const app: Express = express();
 const port = process.env.PORT || 3001;
 const buildPath = path.join(__dirname, '../../build');
 
-// app.get("/", (req: Request, res: Response) => {
-//     res.send("Express + TypeScript Server");
-// });
-
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-});
-
-
 // Middleware
 app.use(bodyParser.json());
-
+app.use(express.static(buildPath));
 
 app.use("/api", apiRouter);
 
@@ -29,11 +20,10 @@ app.get('/api/test', async (_, res) => {
     res.json({ greeting: "Hello AAAAAAAAAA" });
 });
 
-// if (!process.env['VITE']) {
-// const frontendFiles = process.cwd() + '/dist'
-app.use(express.static(buildPath));
 app.get('/*', (_, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
-})
-app.listen(process.env['PORT']);
-// }
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at ${port}`);
+});
